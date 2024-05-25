05c0df9719c230802b18f1685c556032e241ee43
Jamie Frost <jamcris11@hotmail.co.uk>
2024-05-25 16:04:14 +0100

    Alt: test linked_list_test dynamic string

--- tests/linked_list_test.c
+++ tests/linked_list_test.c
@@ -4,20 +4,27 @@ int
 main(int argc, char** argv)
 {
 	struct LinkedList* list;
-	char** data;
+	char* data;
+	char* result;
 
 	list = linked_list_new(sizeof(char*));
 
-	linked_list_add(list, &(char*){"Hello world!"});
-	linked_list_add(list, &(char*){"How are you doing?"});
-	linked_list_add(list, &(char*){"I hope you are doing great!"});
+	data = malloc(sizeof(char) * 6);
+	strcpy(data, "Hello");
+	linked_list_add(list, data);
+	
+	strcpy(data, "Pussy");
+	linked_list_add(list, data);
 
-	linked_list_remove(list, 0);
+	strcpy(data, "Cunny");
+	linked_list_add(list, data);
+	free(data);
 
-	linked_list_reset_iterator(list);
-
-	while ( (data = (char**)linked_list_next(list)) != NULL ) {
-		printf("%s\n", *data);
+	result = linked_list_next(list);
+	
+	while ( result != NULL ) {
+		printf("%s\n", result);
+		result = linked_list_next(list);
 	}
 
 	linked_list_delete(list);
