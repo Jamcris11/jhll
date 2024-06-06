0dd9bf57c1fb2a16e32b555f8282579dc68c78fd
jamcris11 <jamcris11@hotmail.co.uk>
2024-06-06 09:22:59 +0100

    New: XOR swap macro

--- include/util.h
+++ include/util.h
@@ -4,5 +4,9 @@
 #define LEN(x) sizeof(x)/sizeof(x[0])
 #define IS_EVEN(x) x % 2 == 0
 #define IS_BETWEEN(x, y, z) x >= y && x <= z
+#define SWAP(a, b) \
+	a = a^b; \
+	b = a^b; \
+	a = a^b;
 
 #endif // UTIL_H
