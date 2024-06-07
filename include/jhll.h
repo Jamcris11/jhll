2eaf4b75daed55ade3b64bcb42c39189ee814952
jamcris11 <jamcris11@hotmail.co.uk>
2024-06-07 20:32:13 +0100

    Fix: include new headers into parent header

--- include/jhll.h
+++ include/jhll.h
@@ -1,8 +1,11 @@
 #ifndef JHLL_H
 #define JHLL_H
 
+#include "chronos.h"
+#include "hashtable.h"
 #include "linked_list.h"
 #include "list.h"
+#include "log.h"
 #include "queue.h"
 #include "stack.h"
 #include "util.h"
